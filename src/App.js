b9b98ef96923e9b63742d40c8ef8455b0e463c50
vitalyvitmens <otm_@tut.by>
2024-01-15 15:48:49 +0300

    feat(): initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { useState } from 'react'
+
+const FIELDS = [
+  {
+    id: '123',
+    title: 'Name',
+  },
+  {
+    id: '213',
+    title: 'LastName',
+  },
+]
+
+function Item({ name }) {
+  return (
+    <div>
+      <span>{name}</span>
+      <input type="text" />
+    </div>
+  )
+}
 
 function App() {
+  const [items, setItems] = useState(FIELDS)
+
+  const handleClick = () => {
+    setItems([{ id: '323', title: 'New Field' }, ...items])
+    // setItems([items.length, ...items])
+    // setItems([...items, items.length])
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {items.map((item, index) => (
+          <Item key={item.id} name={item.title} />
+        ))}
+        <button onClick={handleClick}>Add</button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
