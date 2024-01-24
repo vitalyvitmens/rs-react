7072dfdf354d0ac4e95e4f6806a0bbe2b850c6a2
vitalyvitmens <otm_@tut.by>
2024-01-24 14:07:36 +0300

    chore(src\SiteBing.jsx): добавляет компонент SiteBing

--- src/index.js
+++ src/index.js
@@ -5,15 +5,17 @@ import LifeCycles from './LifeCycles'
 import Hooks from './Hooks'
 import NestedList from './Checkbox'
 import CustomHooks from './CustomHooks/CustomHooks'
+import SiteBing from './SiteBing'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   // <React.StrictMode>
   <>
-  <CustomHooks/>
+    <CustomHooks />
     {/* <Hooks /> */}
     {/* <LifeCycles /> */}
     {/* <NestedList/> */}
+    {/* <SiteBing/> */}
   </>
   // </React.StrictMode>
 )
